9d16cb90e74c45ab9d18da8cf2637bc165e3e913
AlexWS1048575 <101855082+AlexWS1048575@users.noreply.github.com>
2023-09-26 18:00:20 +0700

    cleaning code

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -13,25 +13,6 @@ use Illuminate\Support\Facades\Auth;
 class ChartController extends Controller
 {
     public function index() {
-
-        // $users = User::select(DB::raw("COUNT(*) as count"), DB::raw("MONTHNAME(created_at) as month_name"))
-        //             ->whereYear('created_at', date('Y'))
-        //             ->groupBy(DB::raw("month_name"))
-        //             ->orderBy('id','ASC')
-        //             ->pluck('count', 'month_name');
-
-        // $users = User::select(DB::raw("COUNT(*) as count"), DB::raw("DAY(created_at) as day"))
-        //             ->whereYear('created_at', date('Y'))
-        //             ->groupBy(DB::raw("day"))
-        //             ->orderBy('id','ASC')
-        //             ->pluck('count', 'day');
-
-        // $corporations = Corporation::select(DB::raw("COUNT(*) as count"), DB::raw("status_id as status"))
-        //             ->LeftJoin('statuses', 'statuses.id', '=', 'corporations.status_id')
-        //             ->groupBy('corporations.status_id')
-        //             ->orderBy('corporations.id','DESC')
-        //             ->pluck('count', 'status');
-
         $corporations = Corporation::select(DB::raw("COUNT(*) as count"), DB::raw("types.name as typename"))
                     ->LeftJoin('types', 'types.id', '=', 'corporations.type_id')
                     ->groupBy('types.name')
@@ -51,10 +32,5 @@ class ChartController extends Controller
         $data2 = $corporations2->values();
         
         return view('chart.pie', compact('labels', 'data', 'labels2', 'data2'));
-
-        // $corporations = DB::table('corporations')
-        //     ->leftJoin('statuses', 'corporations.status_id', '=', 'statuses.id')
-        //     ->get();
-        // return view('chart.pie', compact('corporations'));
     }
 }
